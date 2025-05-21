bac571d6e147f92df51a34d13fd579249f57c3a6
Kyle Berry <kyle.berry@smartwayslfl.com>
2025-05-20 21:50:09 -0500

    chore: remove unused test files and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ErrorBoundary from "./components/ErrorBoundary";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
